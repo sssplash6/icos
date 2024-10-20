7a61fcee2e1d9cca3b6cc219a5152dc91df1ac0e
sssplash6 <probhumanoid@gmail.com>
2024-10-20 15:28:22 +0500

    bgmain

---
+++ script2.js
@@ -0,0 +1,7 @@
+// script.js
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
